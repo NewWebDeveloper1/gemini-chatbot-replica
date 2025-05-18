da58cbe13037f54778d819925aeb9e5cb04a5490
Nitish Kumar <lenitishk@gmail.com>
2025-05-18 22:51:00 +0530

    script changes

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ const fileWrapper = promptForm.querySelector(".file-upload-wrapper");
 const deleteChatButton = document.querySelector("#delete-chat-btn");
 const themeToggleBtn = document.querySelector("#toggle-theme-btn");
 
-const GEMINI_API = API_KEY;
-const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API}`;
+const GEMINI_API_URL = `http://localhost:8000/api/chat`;
 
 let typingInterval, controller;
 let userData = { message: "", file: {} };
@@ -16,10 +15,6 @@ let chatHistory = [];
 
 // Auto-scroll to bottom after every response
 const scrollToBottom = () => {
-  // document.body.scrollTo({
-  //   top: chatsContainer.scrollHeight,
-  //   behavior: "smooth",
-  // });
   chatsContainer.scrollTo({
     top: chatsContainer.scrollHeight,
     behavior: "smooth",
@@ -77,7 +72,7 @@ const generateResponse = async (botMsgDiv) => {
     const response = await fetch(GEMINI_API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ contents: chatHistory }),
+      body: JSON.stringify({ message: chatHistory }),
       signal: controller.signal,
     });
 
@@ -85,7 +80,7 @@ const generateResponse = async (botMsgDiv) => {
     if (!response.ok) throw new Error(data.error.message);
 
     // Process the response text and display it
-    const botResponseText = data.candidates[0].content.parts[0].text
+    const botResponseText = data.reply.candidates[0].content.parts[0].text
       .replace(/\*\*([^*]+)\*\*/g, "$1")
       .replace(/\*([^*]+)/g, "- $1")
       .replace(/\*([^*]+)\*/g, "$1")
